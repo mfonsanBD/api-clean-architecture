b990904abd0567475d3dcdd194d14a2fa034e744
Mike Santos <mike.santos@contraprova.com.br>
2024-06-21 17:53:33 -0300

    test: ensure signup controller returns an erro if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,7 +3,6 @@ import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
-
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -11,9 +10,8 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
-
     const httpResponse = sut.handle(httpRequest)
-
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
