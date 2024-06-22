89436bcee9c8e7057595ba6cdfdc5224fe628460
Mike Santos <mike.santos@contraprova.com.br>
2024-06-21 22:27:46 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -19,11 +19,13 @@ export class SignUpController implements Controller {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email as string)
+      const isValid = this.emailValidator.isValid(email as string)
 
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
